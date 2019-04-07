64f29cd25844e77d2f8feb5edca4aa0c9ee03ae5
Michael Nikitochkin <miry.sof@gmail.com>
2019-04-07 16:41:53 +0200

    Extract to method processing of Items
    Also use pointers instead of object, for easy modification of items and
    share them via heap.

--- go/inventory.go
+++ go/inventory.go
@@ -2,55 +2,59 @@ package main
 
 // StockTaking or "inventory checking" is the physical verification of the quantities and condition of items
 // held in an inventory or warehouse
-func StockTaking(items []Item) []Item {
-	for i := 0; i < len(items); i++ {
-		item := &items[i]
-		if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
-			if item.quality > 0 {
-				if item.name != "Sulfuras, Hand of Ragnaros" {
-					item.quality--
-				}
-			}
-		} else {
-			if item.quality < 50 {
-				item.quality++
-				if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-					if item.sellIn < 11 {
-						if item.quality < 50 {
-							item.quality++
-						}
-					}
-					if item.sellIn < 6 {
-						if item.quality < 50 {
-							item.quality++
-						}
-					}
-				}
+func StockTaking(items []*Item) []*Item {
+	for _, item := range items {
+		processItem(item)
+	}
+
+	return items
+}
+
+func processItem(item *Item) *Item {
+	if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
+		if item.quality > 0 {
+			if item.name != "Sulfuras, Hand of Ragnaros" {
+				item.quality--
 			}
 		}
-
-		if item.name != "Sulfuras, Hand of Ragnaros" {
-			item.sellIn--
-		}
-
-		if item.sellIn < 0 {
-			if item.name != "Aged Brie" {
-				if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-					if item.quality > 0 {
-						if item.name != "Sulfuras, Hand of Ragnaros" {
-							item.quality--
-						}
+	} else {
+		if item.quality < 50 {
+			item.quality++
+			if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+				if item.sellIn < 11 {
+					if item.quality < 50 {
+						item.quality++
 					}
-				} else {
-					item.quality = 0
 				}
-			} else {
-				if item.quality < 50 {
-					item.quality++
+				if item.sellIn < 6 {
+					if item.quality < 50 {
+						item.quality++
+					}
 				}
 			}
 		}
 	}
 
-	return items
+	if item.name != "Sulfuras, Hand of Ragnaros" {
+		item.sellIn--
+	}
+
+	if item.sellIn < 0 {
+		if item.name != "Aged Brie" {
+			if item.name != "Backstage passes to a TAFKAL80ETC concert" {
+				if item.quality > 0 {
+					if item.name != "Sulfuras, Hand of Ragnaros" {
+						item.quality--
+					}
+				}
+			} else {
+				item.quality = 0
+			}
+		} else {
+			if item.quality < 50 {
+				item.quality++
+			}
+		}
+	}
+	return item
 }
--- go/inventory_test.go
+++ go/inventory_test.go
@@ -5,19 +5,19 @@ import (
 )
 
 func TestStockTakingNoPanic(t *testing.T) {
-	items := []Item{
-		Item{"+5 Dexterity Vest", 10, 20},
-		Item{"Aged Brie", 2, 0},
-		Item{"Elixir of the Mongoose", 5, 7},
-		Item{"Sulfuras, Hand of Ragnaros", 0, 80},
-		Item{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
-		Item{"Conjured Mana Cake", 3, 6},
+	items := []*Item{
+		&Item{"+5 Dexterity Vest", 10, 20},
+		&Item{"Aged Brie", 2, 0},
+		&Item{"Elixir of the Mongoose", 5, 7},
+		&Item{"Sulfuras, Hand of Ragnaros", 0, 80},
+		&Item{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+		&Item{"Conjured Mana Cake", 3, 6},
 	}
 	StockTaking(items)
 }
 
 func TestStockTakingEmptyItems(t *testing.T) {
-	subject := []Item{}
+	subject := []*Item{}
 	actual := StockTaking(subject)
 
 	if actual == nil {
@@ -30,8 +30,8 @@ func TestStockTakingEmptyItems(t *testing.T) {
 }
 
 func TestStockTakingDecrSell(t *testing.T) {
-	subject := []Item{
-		Item{"+5 Dexterity Vest", 10, 20},
+	subject := []*Item{
+		&Item{"+5 Dexterity Vest", 10, 20},
 	}
 
 	actual := StockTaking(subject)
@@ -49,23 +49,23 @@ func TestStockTakingChangeQuality(t *testing.T) {
 	subject := "+5 Dexterity Vest"
 	tests := []struct {
 		name     string
-		item     Item
+		item     *Item
 		expected int
 	}{
-		{"quality decrease every day by 1", Item{subject, 15, 20}, 19},
-		{"quality decrease every day by 1 even quality is higher maximum", Item{subject, 15, 60}, 59},
-		{"quality decrease faster on sell passed", Item{subject, -15, 20}, 18},
-		{"quality could be zero", Item{subject, 15, 1}, 0},
-		{"quality could be zero after passed", Item{subject, -15, 2}, 0},
-		{"quality is not negative", Item{subject, 15, 0}, 0},
-		{"quality is not negative even for passed", Item{subject, -15, 0}, 0},
+		{"quality decrease every day by 1", &Item{subject, 15, 20}, 19},
+		{"quality decrease every day by 1 even quality is higher maximum", &Item{subject, 15, 60}, 59},
+		{"quality decrease faster on sell passed", &Item{subject, -15, 20}, 18},
+		{"quality could be zero", &Item{subject, 15, 1}, 0},
+		{"quality could be zero after passed", &Item{subject, -15, 2}, 0},
+		{"quality is not negative", &Item{subject, 15, 0}, 0},
+		{"quality is not negative even for passed", &Item{subject, -15, 0}, 0},
 	}
 
 	for _, tc := range tests {
 		tc := tc // capture range variable
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
-			actual := StockTaking([]Item{tc.item})[0].quality
+			actual := StockTaking([]*Item{tc.item})[0].quality
 			if tc.expected != actual {
 				t.Errorf("Expect quality to be %v for sell in %v. Got %v", tc.expected, tc.item.sellIn, actual)
 			}
@@ -77,29 +77,29 @@ func TestStockTakingCheesItemChangingQuality(t *testing.T) {
 	subject := "Aged Brie"
 	tests := []struct {
 		name     string
-		item     Item
+		item     *Item
 		expected int
 	}{
-		{"quality increase every day by 1", Item{subject, 15, 20}, 21},
+		{"quality increase every day by 1", &Item{subject, 15, 20}, 21},
 		// NOTICE: Spec: Once the sell by date has passed, quality degrades twice as fast.
 		//         It uses word `degrades`, there are no words for Cheese case to `change`
 		// TODO: Clarify specs with product
-		{"quality increase twice on passed", Item{subject, -15, 20}, 22},
-		{"quality could be increased up to 50", Item{subject, -15, 49}, 50},
-		{"quality max 50", Item{subject, -15, 50}, 50},
+		{"quality increase twice on passed", &Item{subject, -15, 20}, 22},
+		{"quality could be increased up to 50", &Item{subject, -15, 49}, 50},
+		{"quality max 50", &Item{subject, -15, 50}, 50},
 		// NOTICE: Spec: The quality of an item is never more than 50.
 		//         There is mention only for Legendary items to not be altered.
 		//         There are no cases, what should we do if there is quality set higher than 50
 		// TODO: Clarify specs with product
-		{"quality should not change on passed and highr max", Item{subject, -15, 60}, 60},
-		{"quality should not change and highr max", Item{subject, 15, 60}, 60},
+		{"quality should not change on passed and highr max", &Item{subject, -15, 60}, 60},
+		{"quality should not change and highr max", &Item{subject, 15, 60}, 60},
 	}
 
 	for _, tc := range tests {
 		tc := tc // capture range variable
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
-			actual := StockTaking([]Item{tc.item})[0].quality
+			actual := StockTaking([]*Item{tc.item})[0].quality
 			if tc.expected != actual {
 				t.Errorf("Expect quality to be %v for sell in %v. Got %v", tc.expected, tc.item.sellIn, actual)
 			}
@@ -108,8 +108,8 @@ func TestStockTakingCheesItemChangingQuality(t *testing.T) {
 }
 
 func TestStockTakingCaseAgedBrieDecrSellIn(t *testing.T) {
-	subject := []Item{
-		Item{"Aged Brie", 10, 30},
+	subject := []*Item{
+		&Item{"Aged Brie", 10, 30},
 	}
 
 	actual := StockTaking(subject)[0].sellIn
@@ -121,8 +121,8 @@ func TestStockTakingCaseAgedBrieDecrSellIn(t *testing.T) {
 }
 
 func TestStockTakingLegendaryItemQualitySame(t *testing.T) {
-	subject := []Item{
-		Item{"Sulfuras, Hand of Ragnaros", 10, 80},
+	subject := []*Item{
+		&Item{"Sulfuras, Hand of Ragnaros", 10, 80},
 	}
 
 	actual := StockTaking(subject)[0].quality
@@ -134,8 +134,8 @@ func TestStockTakingLegendaryItemQualitySame(t *testing.T) {
 }
 
 func TestStockTakingLegendaryItemSellInSame(t *testing.T) {
-	subject := []Item{
-		Item{"Sulfuras, Hand of Ragnaros", 10, 80},
+	subject := []*Item{
+		&Item{"Sulfuras, Hand of Ragnaros", 10, 80},
 	}
 
 	actual := StockTaking(subject)[0].sellIn
@@ -147,8 +147,8 @@ func TestStockTakingLegendaryItemSellInSame(t *testing.T) {
 }
 
 func TestStockTakinBackstagePassesDecrSellIn(t *testing.T) {
-	subject := []Item{
-		Item{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+	subject := []*Item{
+		&Item{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
 	}
 
 	actual := StockTaking(subject)[0].sellIn
@@ -163,22 +163,22 @@ func TestStockTakinBackstagePassesQuality(t *testing.T) {
 	backstage := "Backstage passes to a TAFKAL80ETC concert"
 	tests := []struct {
 		name     string
-		item     Item
+		item     *Item
 		expected int
 	}{
-		{"have a lot of time increase by 1", Item{backstage, 15, 20}, 21},
-		{"concert in 11 days increse by 1", Item{backstage, 11, 20}, 21},
-		{"concert in 10 days increse by 2", Item{backstage, 10, 20}, 22},
-		{"concert in 6 days increse by 2", Item{backstage, 6, 20}, 22},
-		{"concert in 5 days increse by 3", Item{backstage, 5, 20}, 23},
-		{"concert pass drop to 0", Item{backstage, -1, 20}, 0},
+		{"have a lot of time increase by 1", &Item{backstage, 15, 20}, 21},
+		{"concert in 11 days increse by 1", &Item{backstage, 11, 20}, 21},
+		{"concert in 10 days increse by 2", &Item{backstage, 10, 20}, 22},
+		{"concert in 6 days increse by 2", &Item{backstage, 6, 20}, 22},
+		{"concert in 5 days increse by 3", &Item{backstage, 5, 20}, 23},
+		{"concert pass drop to 0", &Item{backstage, -1, 20}, 0},
 	}
 
 	for _, tc := range tests {
 		tc := tc // capture range variable
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
-			actual := StockTaking([]Item{tc.item})[0].quality
+			actual := StockTaking([]*Item{tc.item})[0].quality
 			if tc.expected != actual {
 				t.Errorf("Expect quality to be %v for sell in %v. Got %v", tc.expected, tc.item.sellIn, actual)
 			}
@@ -191,24 +191,24 @@ func TestStockTakinConjuredGeneralQualities(t *testing.T) {
 	subject := "Conjured Mana Cake"
 	tests := []struct {
 		name     string
-		item     Item
+		item     *Item
 		expected int
 	}{
-		{"quality decrease every day by 2", Item{subject, 15, 20}, 18},
-		{"quality decrease every day by 2 even quality is higher maximum", Item{subject, 15, 60}, 58},
-		{"quality decrease faster on sell passed", Item{subject, -15, 20}, 16},
-		{"quality could be zero", Item{subject, 15, 2}, 0},
-		{"quality could be zero with current quality 1", Item{subject, 15, 1}, 0},
-		{"quality could be zero after passed", Item{subject, -15, 4}, 0},
-		{"quality is not negative", Item{subject, 15, 1}, 0},
-		{"quality is not negative even for passed", Item{subject, -15, 0}, 0},
+		{"quality decrease every day by 2", &Item{subject, 15, 20}, 18},
+		{"quality decrease every day by 2 even quality is higher maximum", &Item{subject, 15, 60}, 58},
+		{"quality decrease faster on sell passed", &Item{subject, -15, 20}, 16},
+		{"quality could be zero", &Item{subject, 15, 2}, 0},
+		{"quality could be zero with current quality 1", &Item{subject, 15, 1}, 0},
+		{"quality could be zero after passed", &Item{subject, -15, 4}, 0},
+		{"quality is not negative", &Item{subject, 15, 1}, 0},
+		{"quality is not negative even for passed", &Item{subject, -15, 0}, 0},
 	}
 
 	for _, tc := range tests {
 		tc := tc // capture range variable
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
-			actual := StockTaking([]Item{tc.item})[0].quality
+			actual := StockTaking([]*Item{tc.item})[0].quality
 			if tc.expected != actual {
 				t.Errorf("Expect quality to be %v for sell in %v. Got %v", tc.expected, tc.item.sellIn, actual)
 			}
