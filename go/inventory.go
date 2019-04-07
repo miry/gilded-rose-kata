42234abc21017f11bd7bb13059b2a373d6c24547
Michael Nikitochkin <miry.sof@gmail.com>
2019-04-07 16:31:05 +0200

    Remove duplication inside loop
    In the loop of checking each item, replaced access by index, with
    pointer to object and work with it. It would give us next step to
    extract loop logic to separate function.

--- go/inventory.go
+++ go/inventory.go
@@ -4,49 +4,49 @@ package main
 // held in an inventory or warehouse
 func StockTaking(items []Item) []Item {
 	for i := 0; i < len(items); i++ {
-
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
+		item := &items[i]
+		if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
+			if item.quality > 0 {
+				if item.name != "Sulfuras, Hand of Ragnaros" {
+					item.quality--
 				}
 			}
 		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality++
+			if item.quality < 50 {
+				item.quality++
+				if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+					if item.sellIn < 11 {
+						if item.quality < 50 {
+							item.quality++
 						}
 					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality++
+					if item.sellIn < 6 {
+						if item.quality < 50 {
+							item.quality++
 						}
 					}
 				}
 			}
 		}
 
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
+		if item.name != "Sulfuras, Hand of Ragnaros" {
+			item.sellIn--
 		}
 
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
+		if item.sellIn < 0 {
+			if item.name != "Aged Brie" {
+				if item.name != "Backstage passes to a TAFKAL80ETC concert" {
+					if item.quality > 0 {
+						if item.name != "Sulfuras, Hand of Ragnaros" {
+							item.quality--
 						}
 					}
 				} else {
-					items[i].quality = 0
+					item.quality = 0
 				}
 			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
+				if item.quality < 50 {
+					item.quality++
 				}
 			}
 		}
